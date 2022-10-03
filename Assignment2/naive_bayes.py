eb1853e77732b3ccea50b2240bb722a4550885ec
RaulGuerra <89431126+RaulGuerra@users.noreply.github.com>
2022-10-02 22:47:14 -0700

    Update naive_bayes.py

--- Assignment2/naive_bayes.py
+++ Assignment2/naive_bayes.py
@@ -1,9 +1,9 @@
 #-------------------------------------------------------------------------
-# AUTHOR: your name
-# FILENAME: title of the source file
-# SPECIFICATION: description of the program
+# AUTHOR: Raul Guerra
+# FILENAME: naive_bayes.py
+# SPECIFICATION: makes prediction using naive bayes
 # FOR: CS 4210- Assignment #2
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 2 hours
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard
